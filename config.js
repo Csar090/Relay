83f290a0c09e8174f518852ec748b1233f267d60
Csar090 <sarveshton@gmail.com>
2026-09-21 00:25:28 +0530

    Configure Relay backend endpoint

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.REL_CONFIG = Object.freeze({
-  // Paste the Relationship System Apps Script /exec URL here after deploying the API-enabled Code.gs.
-  backendUrl: ""
+  backendUrl: "https://script.google.com/macros/s/AKfycbz361WCSr8liEDp1zbWTlr2cnScNWqM3YHNE9Xt80ZDqtJSET-WxCBlHDKug5Jl_szR/exec"
 });
